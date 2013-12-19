02e89a1f84da95f60d7b2e15559ee8de11679c39
Jongmin Kim <atomaths@gmail.com>
2013-12-19 11:53:48 +0900

    solve 0000-festival in Go

--- 0000-festival/go/solve1.go
+++ 0000-festival/go/solve1.go
@@ -1,23 +1,59 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"os"
+        "fmt"
 )
 
 func main() {
-	f, err := os.Open("1.in")
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
+        var cases int
+        var rental, team int
 
-	scanner := bufio.NewScanner(f)
-	for scanner.Scan() {
-		fmt.Println(scanner.Text()) // Println will add back the final '\n'
-	}
-	if err := scanner.Err(); err != nil {
-		fmt.Fprintln(os.Stderr, "reading standard input:", err)
-	}
+        fmt.Scanf("%d", &cases)
+
+        for cases > 0 {
+                fmt.Scanf("%d%d", &rental, &team)
+
+                price := make([]int, rental)
+                for i := 0; i < rental; i++ {
+                        fmt.Scanf("%d", &price[i])
+                }
+
+                // 가장 저렴한 대여료가 되는 페스티벌 시작-끝 날짜 확인
+                // startDay, endDay, min := minPrice(rental, team, price)
+                // fmt.Printf("(%d - %d): %.11f\n", startDay, endDay, min)
+                _, _, min := minPrice(rental, team, price)
+                fmt.Printf("%.11f\n", min)
+
+                cases--
+        }
+}
+
+func minPrice(rental, team int, price []int) (startDay, endDay int, min float32) {
+        min = 3.0
+
+        for rental >= team {
+                offset := 0
+                end := team
+
+                for rental >= end {
+                        tmp := float32(sum(price[offset:end])) / float32(team)
+                        if tmp < min {
+                                startDay = offset + 1
+                                endDay = end
+                                min = tmp
+                        }
+                        offset++
+                        end++
+                }
+
+                team++
+        }
+        return
+}
+
+func sum(price []int) (sum int) {
+        for _, v := range price {
+                sum += v
+        }
+        return sum
 }
